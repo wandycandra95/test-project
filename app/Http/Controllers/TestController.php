818f7d2ae2abfe47ca552cb64d62b86c6b2b18c9
wandycandra <57247811+wandycandra@users.noreply.github.com>
2021-08-06 10:59:19 +0700

    Update TestController.php
    abcd

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,15 +7,11 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
  public function test(){
-    print("something")
-    print("abcd")
+    print("something");
+    print("abcd");
  }
 
     public function test2(){
-        print("a);
-    }
-  
-    public function test2(){
-    
+        print("a");
     }
 }
