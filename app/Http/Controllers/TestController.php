186be9bb63afa1b7cc473c477a56b1e2f18ac462
wandycandra <57247811+wandycandra@users.noreply.github.com>
2021-08-06 10:24:29 +0700

    Merge branch 'master' into pr/6

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,8 +7,7 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
  public function test(){
-    print("something") 
-    print("abcd")
- }
- 
+    print("something");
+    print("abcd");
+    }
 }
